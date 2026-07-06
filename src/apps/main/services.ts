93f036051e93983bac9e8b433d4b429aa03d4ef3
speckij <j.speckamp@52North.org>
2026-07-06 14:18:22 +0200

    fix wmts url

--- src/apps/main/services.ts
+++ src/apps/main/services.ts
@@ -360,7 +360,7 @@ export class MainMapProvider implements MapConfigProvider {
 
                         source: new XYZ({
                             // Replace this with your actual server URL
-                            url: "http://85.14.37.51/tiles/aero_3857/{z}/{x}/{y}.png",
+                            url: "https://gis.mrrb.government.bg/tiles/aero_3857/{z}/{x}/{y}.png",
                         }),
                         properties: { title: "Bulgaria" }
                     }),
